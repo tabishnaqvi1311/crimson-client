fdb658aa715d0aac859bcfdc394b5cd81c09a342
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-03-14 14:14:42 +0530

    minor ui fix in youtuber profile page

--- src/components/profile/youtuber/youtuber-profile.tsx
+++ src/components/profile/youtuber/youtuber-profile.tsx
@@ -21,11 +21,11 @@ export default function YoutuberProfile({
                 icon="👋"
                 me={user.id === userId}
             />
-            <div className="w-[95%] flex justify-between items-center mt-6">
+            {user.postedJobs && user.postedJobs?.length > 0 && <div className="w-[95%] flex justify-between items-center mt-6">
                 <h1 className={`${bricolageGrotesqueBold.className} text-xl  `}>Recently Posted Jobs</h1>
                 {/* TODO: redirect talent to filtered jobs page */}
                 {user.id === userId && <Link href={"/jobs"} className={`${spaceGroteskMedium.className} text-primary`}>View all &rarr;</Link>}
-            </div>
+            </div>}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2 w-[95%]">{
                 user.postedJobs && user.postedJobs.map((job: Job) => (
                     <JobDrawer key={job.id} job={job}>
